388052839d30b2e9257f1fb547a1efea80f56647
Ariel11312 <ariellabuson11@gmail.com>
2025-03-09 05:16:24 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 
 // CORS configuration
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'https://wemultiplyapp.com',
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization'
@@ -39,7 +39,7 @@ app.use(cors({
 
 // Handle Preflight Requests for CORS
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL || 'http://localhost:3000');
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL || 'https://wemultiplyapp.com');
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
