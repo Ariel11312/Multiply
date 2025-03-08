c290ed583bc347a59ffc4c6dbf6349e92786ffcc
Ariel11312 <ariellabuson11@gmail.com>
2025-03-09 04:41:15 +0800

    Update cart.js

--- backend/models/cart.js
+++ backend/models/cart.js
@@ -9,20 +9,20 @@ const cartSchema = new mongoose.Schema({
       itemId: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Item',
-        required: true
+
       },
       name: {
         type: String,
-        required: true
+
       },
       quantity: { 
         type: Number, 
-        required: true,
+,
         min: 1
       },
       price: { 
         type: Number, 
-        required: true 
+ 
       },
       imageUrl: {
         type: String
